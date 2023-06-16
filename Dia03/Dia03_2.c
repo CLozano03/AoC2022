cdcd4f1543c717b5d0fe298806f3e6cf0d980cfa
SesarLosano <cesar.lozano.argueso@gmail.com>
2023-06-16 17:32:49 +0200

    Dia 03: C :star: :star:

--- Dia03/Dia03_2.c
+++ Dia03/Dia03_2.c
@@ -1,49 +1,55 @@
 /**
- *  Compilar: gcc Dia03_1.c -o Dia03_1
- *  Ejecutar: Dia03_1.exe input.txt
+ *  Compilar: gcc Dia03_2.c -o Dia03_2
+ *  Ejecutar: Dia03_2.exe input.txt
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_LINEA 2048
+#define MAX_LINEA 1024
 
 int sum_priorities(FILE* f){
     /* Variables necesarias para el proceso */
-    char linea[MAX_LINEA];
-    char compartment1[MAX_LINEA/2], compartment2[MAX_LINEA/2];
-    char c_in_each;
-    int suma_priorities = 0, linea_len, i, j;
+    char linea1[MAX_LINEA], linea2[MAX_LINEA], linea3[MAX_LINEA];
+    char c_in_all;
+    int suma_item_types = 0, linea1_len, linea2_len, linea3_len, i, j, k;
     int found;
     
-    while(fgets(linea, MAX_LINEA, f)){
+    /* Se asume por el enunciado que el archivo input tiene un numero de lineas multiplo de 3 */
+    while(fgets(linea1, MAX_LINEA, f)){
+        fgets(linea2, MAX_LINEA, f);
+        fgets(linea3, MAX_LINEA, f);
         found = 0;
         /* Cada compartment equivale a un substring de la mitad de la linea */ 
-        linea_len = strlen(linea);
-        strncpy(compartment1, linea, linea_len/2);        
-        strncpy(compartment2, linea + linea_len/2, linea_len/2);
+        linea1_len = strlen(linea1);
+        linea2_len = strlen(linea2);
+        linea3_len = strlen(linea3);
 
         /* Busco el caracter que se repite en ambos compartimentos */
-        for(i = 0; i < linea_len/2 && found == 0; i ++){
-            for(j = 0; j < linea_len/2 && found == 0; j++){
-                if(compartment1[i] == compartment2[j]){
-                    c_in_each = compartment1[i];
-                    found = 1;
+        for(i = 0; i < linea1_len && found == 0; i ++){
+            for(j = 0; j < linea2_len && found == 0; j++){
+                if(linea1[i] == linea2[j]){
+                    c_in_all = linea1[i];
+                    for(k = 0; k < linea3_len && found == 0; k++){
+                        if(c_in_all == linea3[k])
+                            found = 1;
+                    }
                 }
             }
         }
         /* 
+            Asigno las prioridades a los item types:
             - Lowercase item types a through z have priorities 1 through 26.
             - Uppercase item types A through Z have priorities 27 through 52.
          */
-        if (c_in_each >= 'a' && c_in_each <= 'z') {
-            suma_priorities += c_in_each - 'a' + 1;
-        } else if (c_in_each >= 'A' && c_in_each <= 'Z') {
-            suma_priorities += c_in_each - 'A' + 27;
+        if (c_in_all >= 'a' && c_in_all <= 'z') {
+            suma_item_types += c_in_all - 'a' + 1;
+        } else if (c_in_all >= 'A' && c_in_all <= 'Z') {
+            suma_item_types += c_in_all - 'A' + 27;
         }
     }
-    return suma_priorities;
+    return suma_item_types;
 }
 
 /* PROGRAMA PRINCIPAL */
