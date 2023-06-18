45c529a88f1e76250ded5d8314f2f5e70f594eaf
SesarLosano <cesar.lozano.argueso@gmail.com>
2023-06-18 13:42:24 +0200

    Dia 08: C :star: :star:

--- Dia08/Dia08_2.c
+++ Dia08/Dia08_2.c
@@ -1,68 +1,75 @@
 /**
- *  Compilar: gcc Dia08_1.c -o Dia08_1
- *  Ejecutar: Dia08_1.exe input.txt
+ *  Compilar: gcc Dia08_2.c -o Dia08_2
+ *  Ejecutar: Dia08_2.exe input.txt
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-/* A simple vista, vemos que en el input nos ponen un bosque
-cuadrado de 99x99 que introduciremos en una matriz */
+/* Tamaño de la matriz */
 #define FOREST_SIZE 99
 #define true 1
 #define false 0
 
 /* Cabeceras */
-int visible_from_outside(int** forest, int x, int y);
+int calculate_scenic_score(int** forest, int x, int y);
 
-int count_visible_trees(int** forest) {
-    int result = 0, i, j;
+int max_scenic_score(int** forest) {
+    int result = -1, actual, i, j;
 
     for(i = 0; i < FOREST_SIZE; i++){
         for(j = 0; j < FOREST_SIZE; j++){
-            if(visible_from_outside(forest, i, j)){
-                result++;
+            actual = calculate_scenic_score(forest, i, j);
+            if(actual > result){
+                result = actual;
             }
         }
     }
     return result;
 }       
 
-int visible_from_outside(int** forest, int x, int y){
+int calculate_scenic_score(int** forest, int x, int y){
     /* Defino resultado para cualquier posible punto cardinal */
-    int result_h_1 = true, result_h_2 = true;
-    int result_v_1 = true, result_v_2 = true;
+    int result_h_1 = 0, result_h_2 = 0;
+    int result_v_1 = 0, result_v_2 = 0;
+    int stop = false;
 
-    int i, j, actual_tree = -1;
-    int tree_size = forest[x][y]; /* Arbol que queremos comprobar si es visible */ 
+    int i, j;
+    int tree_size = forest[x][y]; /* Arbol que queremos comparar */ 
 
     /* ----Comprobacion horizontal---- */
     /* IZQUIERDA del arbol */
-    for(j = y - 1; j > -1 && result_h_1; j--){
+    for(j = y - 1; j > -1 && !stop; j--){
+        result_h_1++;
         if(forest[x][j] >= tree_size)
-            result_h_1 = false;
+            stop = true;
     }
     /* DERECHA del arbol */
-    for(j = y + 1; j < FOREST_SIZE && result_h_2; j++){
+    stop = false;
+    for(j = y + 1; j < FOREST_SIZE && !stop; j++){
+        result_h_2++;
         if(forest[x][j] >= tree_size)
-            result_h_2 = false;
+            stop = true;
     }
 
     /* ----Comprobacion vertical---- */
     /* ARRIBA del arbol */
-    for(i = x - 1; i > -1 && result_v_1; i--){
+    stop = false;
+    for(i = x - 1; i > -1 && !stop; i--){
+        result_v_1++;
         if(forest[i][y] >= tree_size)
-            result_v_1 = false;
+            stop = true;
     }
     /* DEBAJO del arbol */
-    for(i = x + 1; i < FOREST_SIZE && result_v_2; i++){
+    stop = false;
+    for(i = x + 1; i < FOREST_SIZE && !stop; i++){
+        result_v_2++;
         if(forest[i][y] >= tree_size)
-            result_v_2 = false;
+            stop = true;
     }
 
-    return result_h_1 || result_v_1 || result_h_2 || result_v_2;
+    return result_h_1 * result_v_1 * result_h_2 * result_v_2;
 }
-
 
 void parse_file(FILE* f, int** forest){
     int i, j;
@@ -99,12 +106,12 @@ int main(int argc, char* argv[]){
     /* Parseo de archivo */
     parse_file(f, forest);
     fclose(f); 
-    
+
     /* Operacion de cuenta */
-    int visible_trees = count_visible_trees(forest);
+    int max_score = max_scenic_score(forest);
 
     /* Imprimir resultado */
-    printf("\nArboles visibles: %d\n", visible_trees);
+    printf("\nArboles visibles: %d\n", max_score);
 
     /* Liberar memoria asignada al bosque */
     for (i = 0; i < FOREST_SIZE; i++) {
